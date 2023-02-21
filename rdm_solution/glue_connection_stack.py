7a42789c950d4f1b233ba8f929e7ee49049ff4ef
Dhanasekaran K <2000080515@hexaware.com>
2023-02-21 15:31:00 +0530

    updated

--- rdm_solution/glue_connection_stack.py
+++ rdm_solution/glue_connection_stack.py
@@ -47,7 +47,17 @@ class GlueConnectionStack(Stack):
         vpc = ec2.Vpc(
             self, "rdmVPC", 
             max_azs=2,
-            nat_gateways=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public", cidr_mask=24, subnet_type=ec2.SubnetType.PUBLIC
+                ),
+                ec2.SubnetConfiguration(
+                    name="app", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE
+                ),
+                ec2.SubnetConfiguration(
+                    name="db", cidr_mask=24, subnet_type=ec2.SubnetType.ISOLATED
+                )
+            ]
         )
         sg = ec2.SecurityGroup(
             self, 
@@ -60,7 +70,7 @@ class GlueConnectionStack(Stack):
             ec2.Peer.ipv4('0.0.0.0/0'),
             ec2.Port.tcp(5432)
         )
-
+        
         # Create the Glue Connection
         glue_connection = glue.CfnConnection(
             self, "rdm_connection",
