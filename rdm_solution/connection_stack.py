3f77304613287c680e65532e8a4bd5af3cfc852c
Dhanasekaran K <2000080515@hexaware.com>
2023-02-21 13:26:14 +0530

    updated

--- rdm_solution/connection_stack.py
+++ rdm_solution/connection_stack.py
@@ -74,11 +74,7 @@ class ConnectionStack(Stack):
                     'JDBC_ENFORCE_SSL': 'false',
                     'USERNAME' : 'rdm_admin',
                     'PASSWORD': 'RDMadmin2023#'
-                },
-                "physical_connection_requirements" : {
-                    "subnet_id": vpc.public_subnets[0].subnet_id,
-                    "security_group_id_list": [sg.security_group_id],
-                },
+                }
             },
         )
 
