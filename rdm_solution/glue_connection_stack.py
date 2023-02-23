13d2a157f22971595afb0b5bf310120325f81a3b
Dhanasekaran K <2000080515@hexaware.com>
2023-02-23 17:15:14 +0530

    multiple table mappings added

--- rdm_solution/glue_connection_stack.py
+++ rdm_solution/glue_connection_stack.py
@@ -21,16 +21,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-
-
-mapping_dict = [{
-    "table_name":"postgres_dev_country_info",
-    "s3_path": "s3://scheme-ref-dev-data-copy/COUNTRY.txt",
-    "field_mapping":[
-        ("COUNTRY_CODE", "string", "COUNTRY_CODE", "string"),
-        ("COUNTRY_NAME", "string", "COUNTRY_NAME", "string"),
-    ]
-}]
+import json
+import boto3
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -39,42 +31,48 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
+s3 = boto3.client('s3')
+response = s3.get_object(Bucket="etl-glue-scripts", Key="mappings.json")
+json_data = json.loads(response['Body'].read().decode('utf-8'))
 
-for item in mapping_dict:
-    # Script generated for node S3 bucket
-    S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
-        format_options={
-            "quoteChar": '"',
-            "withHeader": True,
-            "separator": "|",
-            "optimizePerformance": False,
-        },
-        connection_type="s3",
-        format="csv",
-        connection_options={
-            "paths": [item["s3_path"]],
-            "recurse": True,
-        },
-        transformation_ctx="S3bucket_node1",
-    )
+for i, item in enumerate(json_data):
+    if json_data["is_migrating"] == 1:
+        S3bucket_node = f'S3bucket_node{i+1}'
+        ApplyMapping_node = f'ApplyMapping_node{i+2}'
+        PostgreSQLtable_node = f'PostgreSQLtable_node{i+3}'
+        # Script generated for node S3 bucket
+        S3bucket_node = glueContext.create_dynamic_frame.from_options(
+            format_options={
+                "quoteChar": '"',
+                "withHeader": True,
+                "separator": "|",
+                "optimizePerformance": False,
+            },
+            connection_type="s3",
+            format="csv",
+            connection_options={
+                "paths": [item["s3_path"]],
+                "recurse": True,
+            },
+            transformation_ctx=S3bucket_node,
+        )
+        _field_mapping = [tuple(_item) for _item in item["field_mapping"]]
+        # Script generated for node ApplyMapping
+        ApplyMapping_node = ApplyMapping.apply(
+            frame=S3bucket_node,
+            mappings=_field_mapping,
+            transformation_ctx=ApplyMapping_node,
+        )
 
-    # Script generated for node ApplyMapping
-    ApplyMapping_node2 = ApplyMapping.apply(
-        frame=S3bucket_node1,
-        mappings=item["field_mapping"],
-        transformation_ctx="ApplyMapping_node2",
-    )
+        # Script generated for node PostgreSQL table
+        PostgreSQLtable_node = glueContext.write_dynamic_frame.from_catalog(
+            frame=ApplyMapping_node,
+            database="rdm_solution_glue_database",
+            table_name=item["table_name"],
+            transformation_ctx=PostgreSQLtable_node,
+        )
 
-    # Script generated for node PostgreSQL table
-    PostgreSQLtable_node3 = glueContext.write_dynamic_frame.from_catalog(
-        frame=ApplyMapping_node2,
-        database="rdm_solution_glue_database",
-        table_name=item["table_name"],
-        transformation_ctx="PostgreSQLtable_node3",
-    )
-
-job.commit()
-
+        job.commit()
 
 """
 
